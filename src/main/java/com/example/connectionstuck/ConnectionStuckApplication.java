f86744edb92acf9ef726a6b5c2642eb801bb00be
jfrossetto <jfrossetto@gmail.com>
2021-10-26 11:16:35 -0300

    logs

--- src/main/java/com/example/connectionstuck/ConnectionStuckApplication.java
+++ src/main/java/com/example/connectionstuck/ConnectionStuckApplication.java
@@ -8,7 +8,7 @@ public class ConnectionStuckApplication {
 
   public static void main(String[] args) {
 
-    System.setProperty("logging.level.io.r2dbc.postgresql", "ERROR");
+    System.setProperty("logging.level.io.r2dbc.postgresql", "DEBUG");
     System.setProperty("logging.level", "TRACE");
     System.setProperty("logging.level.io.r2dbc.pool", "TRACE");
     SpringApplication.run(ConnectionStuckApplication.class, args);
--- src/main/java/com/example/connectionstuck/PostgresR2dbcConnectionPoolFactory.java
+++ src/main/java/com/example/connectionstuck/PostgresR2dbcConnectionPoolFactory.java
@@ -4,10 +4,15 @@ import io.r2dbc.pool.ConnectionPool;
 import io.r2dbc.pool.ConnectionPoolConfiguration;
 import io.r2dbc.postgresql.PostgresqlConnectionConfiguration;
 import io.r2dbc.postgresql.PostgresqlConnectionFactory;
+import io.r2dbc.spi.ConnectionFactories;
 import io.r2dbc.spi.ConnectionFactory;
+import io.r2dbc.spi.ConnectionFactoryOptions;
+
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
+
+import static io.r2dbc.spi.ConnectionFactoryOptions.*;
 
 public class PostgresR2dbcConnectionPoolFactory {
 
@@ -57,5 +62,17 @@ public class PostgresR2dbcConnectionPoolFactory {
             .maxLifeTime(Duration.ofMillis(Long.parseLong(maxLifetime)))
             .validationQuery("SELECT 1")
             .build());
+
+    /* too many clients
+    return ConnectionFactories.get(ConnectionFactoryOptions.builder()
+            .option(DRIVER,"postgresql")
+            .option(HOST,"localhost")
+            .option(PORT,5432)
+            .option(USER,"postgres")
+            .option(PASSWORD,"admin")
+            .option(DATABASE,"postgres")
+            .build());
+    */
+
   }
 }
