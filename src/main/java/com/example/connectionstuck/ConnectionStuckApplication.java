6aac02267197ce00cb14f5df07458813b9c6f2b8
jfrossetto <jfrossetto@gmail.com>
2021-10-29 16:34:45 -0300

    removed spring-data

--- src/main/java/com/example/connectionstuck/ConnectionStuckApplication.java
+++ src/main/java/com/example/connectionstuck/ConnectionStuckApplication.java
@@ -1,9 +1,15 @@
 package com.example.connectionstuck;
 
+import io.r2dbc.spi.Connection;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.transaction.TransactionAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class})
 public class ConnectionStuckApplication {
 
   public static void main(String[] args) {
